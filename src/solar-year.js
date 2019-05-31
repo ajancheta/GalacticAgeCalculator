5f6fc373dfccc6fcf28797c21ef44effab69b396
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-31 16:05:59 -0700

    dry up functions

--- src/solar-year.js
+++ src/solar-year.js
@@ -5,37 +5,37 @@ export default class SolarAgeCalculator {
 
   mercury(){
     let mercuryYear = 0.24;
-    return (this.year * mercuryYear);
+    return ((this.year * mercuryYear).toFixed(2));
   }
 
   lifeExpMercury(){
-    return(24 - this.year * 0.24);
+    return((24 - this.year * 0.24).toFixed(2));
   }
 
   venus(){
     let venusYear = 0.62;
-    return (this.year*venusYear);
+    return ((this.year*venusYear).toFixed(2));
   }
 
   lifeExpVenus(){
-    return(62 - this.year * 0.62);
+    return((62 - this.year * 0.62).toFixed(2));
   }
 
   mars(){
     let marsYear = 1.88;
-    return (this.year * marsYear);
+    return((this.year * marsYear).toFixed(2));
   }
 
   lifeExpMars(){
-    return(188 - this.year * 1.88);
+    return((188 - this.year * 1.88).toFixed(2));
   }
 
   jupiter(){
     let jupiterYear = 11.86;
-    return (this.year * jupiterYear);
+    return((this.year * jupiterYear).toFixed(2));
   }
 
   lifeExpJupiter(){
-    return(1186 - this.year * 11.86);
+    return((1186 - this.year * 11.86).toFixed(2));
   }
 }
