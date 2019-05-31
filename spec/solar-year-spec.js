b98432552fd9b2053bb6d3bcfd42a9f00cd6ba3a
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-31 10:03:53 -0700

    add passing tests for mercury, venus, mars and jupiter years

--- spec/solar-year-spec.js
+++ spec/solar-year-spec.js
@@ -1,8 +1,23 @@
 import SolarAgeCalculator from './../src/solar-year'
 
-describe('Mercury', function () {
+describe('Solar Age Calculator', function () {
   it('should test that the calculated Mercury age is correct', function () {
     let userAge = new SolarAgeCalculator(1);
     expect(userAge.mercury()).toEqual(0.24);
+  });
+
+  it('should test that the calculated Venus age is correct', function () {
+    let userAge = new SolarAgeCalculator(1);
+    expect(userAge.venus()).toEqual(0.62);
+  });
+
+  it('should test that the calculated Mars age is correct', function () {
+    let userAge = new SolarAgeCalculator(1);
+    expect(userAge.mars()).toEqual(1.88);
+  });
+
+  it('should test that the calculated Jupiter age is correct', function () {
+    let userAge = new SolarAgeCalculator(1);
+    expect(userAge.jupiter()).toEqual(11.86);
   });
 });
