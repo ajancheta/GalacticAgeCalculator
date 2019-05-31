d0cafc3361af5d93aa98dbf13835b1172ac8eacd
AJ Ancheta <ashleyjancheta@gmail.com>
2019-05-31 10:04:18 -0700

    add same logic of mercury years to venus, mars, and jupiter years

--- src/solar-year.js
+++ src/solar-year.js
@@ -4,7 +4,22 @@ export default class SolarAgeCalculator {
   }
 
   mercury(){
-    const mercuryYear = 0.24;
+    let mercuryYear = 0.24;
     return (this.year*mercuryYear);
+  }
+
+  venus(){
+    let venusYear = 0.62;
+    return (this.year*venusYear);
+  }
+
+  mars(){
+    let marsYear = 1.88;
+    return (this.year*marsYear);
+  }
+
+  jupiter(){
+    let jupiterYear = 11.86;
+    return (this.year*jupiterYear);
   }
 }
